3820598ffc6d6c555241b3c73338be92620d8db8
zulroxx <zulkiflikyky@gmail.com>
2024-10-21 11:51:28 +0700

    Update logo

--- src/common/theme/palette.js
+++ src/common/theme/palette.js
@@ -8,10 +8,10 @@ export default (server, darkMode) => ({
     default: darkMode ? grey[900] : grey[50],
   },
   primary: {
-    main: validatedColor(server?.attributes?.colorPrimary) || (darkMode ? indigo[200] : indigo[900]),
+    main: validatedColor(server?.attributes?.colorPrimary) || '#0078f0',
   },
   secondary: {
-    main: validatedColor(server?.attributes?.colorSecondary) || (darkMode ? green[200] : green[800]),
+    main: validatedColor(server?.attributes?.colorSecondary) || '#00478f',
   },
   neutral: {
     main: grey[500],
--- src/login/LogoImage.jsx
+++ src/login/LogoImage.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { useTheme, useMediaQuery } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { makeStyles } from '@mui/styles';
-import Logo from '../resources/images/logo.svg?react';
+// import Logo from '../resources/images/logo.svg?react';
+import Logo from '/pwa-192x192.png';
 
 const useStyles = makeStyles((theme) => ({
   image: {
@@ -15,22 +16,30 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const LogoImage = ({ color }) => {
+// const LogoImage = ({ color }) => {
+//   const theme = useTheme();
+//   const classes = useStyles();
+
+//   const expanded = !useMediaQuery(theme.breakpoints.down('lg'));
+
+//   const logo = useSelector((state) => state.session.server.attributes?.logo);
+//   const logoInverted = useSelector((state) => state.session.server.attributes?.logoInverted);
+
+//   if (logo) {
+//     if (expanded && logoInverted) {
+//       return <img className={classes.image} src={logoInverted} alt="" />;
+//     }
+//     return <img className={classes.image} src={logo} alt="" />;
+//   }
+//   return <Logo className={classes.image} style={{ color }} />;
+// };
+
+const LogoImage = ({color}) => {
   const theme = useTheme();
   const classes = useStyles();
 
-  const expanded = !useMediaQuery(theme.breakpoints.down('lg'));
-
-  const logo = useSelector((state) => state.session.server.attributes?.logo);
-  const logoInverted = useSelector((state) => state.session.server.attributes?.logoInverted);
-
-  if (logo) {
-    if (expanded && logoInverted) {
-      return <img className={classes.image} src={logoInverted} alt="" />;
-    }
-    return <img className={classes.image} src={logo} alt="" />;
-  }
-  return <Logo className={classes.image} style={{ color }} />;
+    return <img className={classes.image} src={Logo} alt="Fleetwize" />
 };
+
 
 export default LogoImage;
